cde2b4c063dd94730886bf42caaf8382d3fd3680
spsteady <spsteady@gmail.com>
2021-03-28 02:11:05 -0500

    Create app.js file
    Create app.js file:
    Initial - variable setup
    Initial - function header setup

---
+++ app.js
@@ -0,0 +1,37 @@
+const qwerty = document.getElementById('qwerty');
+
+const phrase = document.getElementById('phrase');
+
+const resetButton = document.querySelector('.btn_reset');
+
+const missedGuess = 0;
+
+// Return a random phrase from an array
+const getRandomPhraseAsArray = arr => {
+
+}
+
+// Adds the letters of a string to the display
+const addPhraseToDisplay = arr => {
+
+}
+
+// Check if a letter is in the phrase
+const checkLetter = button => {
+
+}
+
+// Check if the game has been won or lost
+const checkWin = () => {
+
+}
+
+// Listen for the start game button to be pressed
+startButton.addEventListener('click', () => {
+    
+});
+
+// Listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', e => {
+
+});
